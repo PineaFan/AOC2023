9c283c50161345d55b709f835393d1435347008e
pineafan <ash@pinea.dev>
2023-12-02 10:15:21 +0000

    Day 2

--- 2/2.ts
+++ 2/2.ts
@@ -9,16 +9,17 @@ for (const line of data) {
     // First, split by ":" to find Game x and the rounds
     const [name, results] = line.split(": ") as [string, string];
     const gameNumber = parseInt(name.split(" ")[1]);
-    let valid = true;
+    let [red, green, blue] = [0, 0, 0]
     for (const result of results.split("; ")) {
         for (const event of result.split(", ")) {
             const [amount, colour] = event.split(" ");
-            if (colour === "red" && parseInt(amount) > 12) valid = false;
-            if (colour === "green" && parseInt(amount) > 13) valid = false;
-            if (colour === "blue" && parseInt(amount) > 14) valid = false;
+            if (colour === "red" && parseInt(amount) > red) red = parseInt(amount);
+            if (colour === "green" && parseInt(amount) > green) green = parseInt(amount);
+            if (colour === "blue" && parseInt(amount) > blue) blue = parseInt(amount);
         }
     }
-    if (valid) sum += gameNumber
+    const power = red * green * blue;
+    sum += power
 }
 
 console.log(sum)
