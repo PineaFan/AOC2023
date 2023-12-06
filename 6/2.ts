7aa3095381f99701efdbbc67db1f401cb39ad950
pineafan <ash@pinea.dev>
2023-12-06 16:35:43 +0000

    Day 6 - 0.02s for part 2 isn't bad!

---
+++ 6/2.ts
@@ -0,0 +1,21 @@
+import { readFileSync } from "fs";
+
+const [rawTimes, rawDistances] = readFileSync("6/data.txt", "utf-8").split("\n").filter((line) => line.length)
+const time = parseInt(rawTimes.split(":")[1].split(" ").filter((x) => x.length).join(""))
+const record = parseInt(rawDistances.split(":")[1].split(" ").filter((x) => x.length).join(""))
+
+// For a time, you can hold for an amount of time (x) anywhere from 0 to the length of the race (length)
+// Each of these hold times gives a distance travelled (y) of speed (time held, x) * time remaining (length - x)
+// So... an equation for the distance travelled is simply (time)(length - time), or timeLength - time^2
+
+// Next, we need to figure out at which point the value is faster than the record
+// So for a distance x, the equation for distance will be:
+// (-time^2) + (time * length) - record > 0
+//     a             b             c
+// So the race will be won between (-b±(b^2 - 4ac)^1/2)/2a (Quadratic Formula)
+const [a, b, c] = [-1, time, -record]
+const sqrtDiscriminant = ((b**2) - (4*a*c))**(1/2)
+const [min, max] = [(-b+sqrtDiscriminant)/(2*a), (-b-sqrtDiscriminant)/(2*a)]
+// The number of whole numbers between min and max is the value we need
+const between = Math.ceil(max) - Math.floor(min) - 1
+console.log(between)
